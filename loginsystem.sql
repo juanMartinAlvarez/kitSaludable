15794978b1f73faf4e4fc2f14c81212ca7108bd6
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-07-03 20:06:08 -0300

    Update loginsystem.sql
    add tables, alimentos alimentosdietas dietas ejercicios ejerciciosrutinas personas rutinas

--- loginsystem.sql
+++ loginsystem.sql
@@ -1,10 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 28, 2020 at 03:39 AM
--- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -53,7 +46,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `lname` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(300) DEFAULT NULL,
-  `contactno` varchar(11) DEFAULT NULL,
   `posting_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -61,9 +53,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `contactno`, `posting_date`) VALUES
-(9, 'Anuj', 'Kumar', 'demouser@gmail.com', 'Test@123', '21222222', '2020-04-15 18:30:00'),
-(11, 'Anuj', 'Kumar', 'phpgurukulofficial@gmail.com', 'Test@123', '1234567890', '2020-04-15 18:30:00');
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `posting_date`) VALUES
+(9, 'Anuj', 'Kumar', 'demouser@gmail.com', 'Test@123', '2020-04-15 18:30:00'),
+(11, 'Anuj', 'Kumar', 'phpgurukulofficial@gmail.com', 'Test@123', '2020-04-15 18:30:00');
 
 --
 -- Indexes for dumped tables
@@ -101,3 +93,153 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `personas`
+--
+CREATE TABLE IF NOT EXISTS `personas` (
+    `id` int(11) NOT NULL,
+    `nombre` varchar(255) NOT NULL,
+    `apellido` varchar(255) NOT NULL,
+    `edad` int NOT NULL,
+    `peso` int(20) NOT NULL,
+    `altura` int(20) NOT NULL,
+    `id_alimentosDietas` int(11),
+    `id_ejerciciosRutinas`int(11),
+    `id_users`int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id`);
+--
+-- AUTO_INCREMENT for table `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `dietas`
+--
+CREATE TABLE IF NOT EXISTS `dietas`(
+    `id` int(11) NOT NULL,
+    `nombre` varchar(255) NOT NULL, 
+    `calorias` varchar(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `dietas`
+--
+ALTER TABLE `dietas`
+    ADD PRIMARY KEY(`id`);
+--
+-- AUTO_INCREMENT for table `dietas`
+--
+ALTER TABLE `dietas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `alimentos`
+--
+CREATE TABLE IF NOT EXISTS `alimentos`(
+    `id` int(11) NOT NULL,
+    `nombre` varchar(255) NOT NULL, 
+    `calorias` int(20) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `alimentos`
+--
+ALTER TABLE `alimentos`
+    ADD PRIMARY KEY(`id`);
+--
+-- AUTO_INCREMENT for table `alimentos`
+--
+ALTER TABLE `alimentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `alimentosDietas`
+--
+CREATE TABLE IF NOT EXISTS `alimentosDietas`(
+    `id` int(11) NOT NULL,
+    `id_dietas` int(11) NOT NULL,
+    `id_alimentos` int(11) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `alimentosDietas`
+--
+ALTER TABLE `alimentosDietas`
+    ADD PRIMARY KEY(`id`);
+--
+-- AUTO_INCREMENT for table `alimentosDietas`
+--
+ALTER TABLE `alimentosDietas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `rutinas`
+--
+CREATE TABLE IF NOT EXISTS `rutinas`(
+    `id` int(11) NOT NULL,
+    `nombre` varchar(255),
+    `intensidad` varchar (255)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `rutinas`
+--
+ALTER TABLE `rutinas`
+    ADD PRIMARY KEY(`id`);
+--
+-- AUTO_INCREMENT for table `rutinas`
+--
+ALTER TABLE `rutinas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `ejercicios`
+--
+CREATE TABLE IF NOT EXISTS `ejercicios`(
+    `id` int(11) NOT NULL,
+    `nombre` varchar(255)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `ejercicios`
+--
+ALTER TABLE `ejercicios`
+    ADD PRIMARY KEY(`id`);
+--
+-- AUTO_INCREMENT for table `ejercicios`
+--
+ALTER TABLE `ejercicios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `usejerciciosRutinasers`
+--
+CREATE TABLE IF NOT EXISTS `ejerciciosRutinas`(
+    `id` int(11) NOT NULL,
+    `id_rutinas` int(11) NOT NULL,
+    `id_ejercicios` int(11) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `usejerciciosRutinasers`
+--
+ALTER TABLE `ejerciciosRutinas`
+    ADD PRIMARY KEY(`id`);
+--
+-- AUTO_INCREMENT for table `usejerciciosRutinasers`
+--
+ALTER TABLE `ejerciciosRutinas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
