d0659d2081d6989624b04a84fea64974778380eb
Fernando <fernandolamasw@hotmail.com>
2020-08-17 20:48:23 -0300

    Arreglado: redirecciona a la página correcta al loguearse, y en caso de no loguear, lo devuelve a la página de login con un aviso de error

--- includes/login.php
+++ includes/login.php
@@ -1,30 +1,29 @@
 <?php
 // Code for login 
-if(isset($_POST['login'])){
-	$password=$_POST['password'];
-	$dec_password=$password;
-	$useremail=$_POST['uemail'];
-	$ret= mysqli_query($con,"SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
-	$num=mysqli_fetch_array($ret);
-	
-	if($num>0){
-		$extra="index.html";
-		$_SESSION['login']=$_POST['uemail'];
-		$_SESSION['id']=$num['id'];
-		$_SESSION['name']=$num['fname'];
-		$host=$_SERVER['HTTP_HOST'];
-		$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+if (isset($_POST['login'])) {
+	$password = $_POST['password'];
+	$dec_password = $password;
+	$useremail = $_POST['uemail'];
+	$ret = mysqli_query($con, "SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
+	$num = mysqli_fetch_array($ret);
+
+	if ($num > 0) {
+		$extra = "menuprincipal.php";
+		$_SESSION['login'] = $_POST['uemail'];
+		$_SESSION['id'] = $num['id'];
+		$_SESSION['name'] = $num['fname'];
+		$host = $_SERVER['HTTP_HOST'];
+		$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 		header("location:http://$host$uri/$extra");
 		exit();
+	} else {
+?>
+		<script language="javascript">
+			alert("Nombre de usuario o contrasenia invalidos");
+			document.location = "index.php";
+		</script>
+<?php
 
-
-	}else{
-		echo "<script>alert('Nombre de usuario o contrasenia invalidos');</script>";
-		$extra="index.php";
-		$host  = $_SERVER['HTTP_HOST'];
-		$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-		//header("location:http://$host$uri/$extra");
-		exit();
 	}
 }
 ?>
