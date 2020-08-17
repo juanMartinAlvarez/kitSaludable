11707a4e079507faa45b5fdfe853ca460ef59ebe
Fernando <fernandolamasw@hotmail.com>
2020-08-17 20:39:57 -0300

    Revert "Merge branch 'master' of https://github.com/juanMartinAlvarez/kitSaludable"
    This reverts commit 6a5facd02c5ce33dd3d99b09d3bdd6cc2d04bd60, reversing
    changes made to 7ab15985b2cd33ae60607862c80a2408d7050843.

--- includes/login.php
+++ includes/login.php
@@ -1,19 +1,19 @@
 <?php
 // Code for login 
-if(isset($_POST['login'])){
-	$password=$_POST['password'];
-	$dec_password=$password;
-	$useremail=$_POST['uemail'];
-	$ret= mysqli_query($con,"SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
-	$num=mysqli_fetch_array($ret);
-	
-	if($num>0){
-		$extra="menuprincipal.php";
-		$_SESSION['login']=$_POST['uemail'];
-		$_SESSION['id']=$num['id'];
-		$_SESSION['name']=$num['fname'];
-		$host=$_SERVER['HTTP_HOST'];
-		$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+if (isset($_POST['login'])) {
+	$password = $_POST['password'];
+	$dec_password = $password;
+	$useremail = $_POST['uemail'];
+	$ret = mysqli_query($con, "SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
+	$num = mysqli_fetch_array($ret);
+
+	if ($num > 0) {
+		$extra = "menuprincipal.php";
+		$_SESSION['login'] = $_POST['uemail'];
+		$_SESSION['id'] = $num['id'];
+		$_SESSION['name'] = $num['fname'];
+		$host = $_SERVER['HTTP_HOST'];
+		$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 		header("location:http://$host$uri/$extra");
 		exit();
 	} else {
@@ -24,14 +24,6 @@ if(isset($_POST['login'])){
 		</script>
 <?php
 
-
-	}else{
-		echo "<script>alert('Nombre de usuario o contrasenia invalidos');</script>";
-		$extra="index.php";
-		$host  = $_SERVER['HTTP_HOST'];
-		$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-		exit();
-		//header("location:http://$host$uri/$extra");
 	}
 }
 ?>
