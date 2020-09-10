67584e67f2a623e79bfbb1e53b443aad0ac895e8
Fernando <fernandolamasw@hotmail.com>
2020-09-10 02:42:32 -0300

    agregados metodos que fueron sacados por accidente

--- persona.php
+++ persona.php
@@ -7,6 +7,8 @@
     private $altura;
     private $edad;
     private $fecha;
+    private $nombre;
+    private $apellido;
 
     public function getid(){
       return $this->id;
@@ -16,6 +18,21 @@
       $this->id = $id;
     }
 
+    public function getNombre(){
+      return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+      $this->nombre = $nombre;
+    }
+
+    public function getApellido(){
+      return $this->apellido;
+    }
+
+    public function setApellido($apellido){
+      $this->apellido = $apellido;
+    }
     public function getpeso(){
       return $this->peso;
     }
