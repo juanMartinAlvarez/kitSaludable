afa327b9316e93b9bafc8d282d352698485627dc
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-09-10 03:40:45 -0300

    Create renovardatos.php

---
+++ renovardatos.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include_once("cruddatos.php");
+
+$sessionid = $_SESSION['id'];
+$peso = $_POST['peso'];
+$altura = $_POST['altura'];
+$edad = $_POST['edad'];
+renovarDatos($peso, $altura, $edad, $sessionid) or die("Error");
+
+?>
