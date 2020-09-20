9034bafdaf9b31427474393c300bf93c706674ac
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-09-20 12:43:29 -0300

    refact tabulacion

--- datos.php
+++ datos.php
@@ -13,9 +13,8 @@
 	</script>
 	<?php
 	session_start();
-
-	include_once("cruddatos.php");
-	require_once('dbconnection.php');
+    include_once("cruddatos.php");
+    require_once('dbconnection.php');
 	?>
 </head>
 
@@ -33,61 +32,18 @@
 				<!-- ** Informacion** -->
 
 				<form name="data-update" method="post" action="renovardatos.php">
-					<input class="form-control" type="number" name="peso" id="peso" size="3" maxlength="4" placeholder="Peso">
-					<input class="form-control" type="number" name="altura" id="altura" size="3" maxlength="3" placeholder="Altura">
-					<input class="form-control" type="number" name="edad" id="edad" size="3" maxlength="3" placeholder="Edad">
+					<input class="form-control" type="number" 
+            name="peso" id="peso" size="3" maxlength="4" placeholder="Peso">
+					<input class="form-control" type="number" 
+            name="altura" id="altura" size="3" maxlength="3" placeholder="Altura">
+					<input class="form-control" type="number" 
+            name="edad" id="edad" size="3" maxlength="3" placeholder="Edad">
+          
 					<input class="btn btn-primary" type="submit" value="Actualizar datos">
 					<input class="btn btn-primary" type="button" value="Mostrar datos" onclick="location.href='?mostrarDatos'">
 					<!--<input class="btn btn-primary" type="button" value="Actualizar datos" onclick="location.href='?actualizarDatos'">-->
 				</form>
-
-				<?php
-				//y esto hay que ver si funciona (lo de abajo)
-				$fila = mostrarDatos();
-				if (isset($_GET['mostrarDatos'])) foreach ($fila as 	$persona) { ?>
-					<td>Id: <?php echo $persona->getid() ?></td><br><br>
-					<td>Peso: <?php echo $persona->getPeso() ?></td><br><br>
-					<td>Altura: <?php echo $persona->getAltura() ?></td><br><br>
-					<td>Edad: <?php echo $persona->getEdad() ?></td><br><br>
-					<td>Nombre: <?php echo $persona->getNombre(); ?></td><br><br>
-					<td>Apellido: <?php echo $persona->getApellido() ?></td> <br> <br>
-					<td>Fecha: <?php echo $persona->getFecha() ?> </td>
-				<?php } ?>
-
-				</row>
-
-				<!-- Modal -->
-				<div class="modal fade" id="Imc" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-					<div class="modal-dialog" role="document">
-						<div class="modal-content" style="height: 700px">
-							<div class="modal-header bg-dark">
-								<h3 class="modal-title text-center" id="ModalLabel">Resultados del calculo del IMC</h3>
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="calcularIMC()">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-
-							<div class="modal-body " style="height: 400px">
-								<row>
-									<div class="col-12 h-10">
-										<p> Ingreso de datos:
-											<input class="text-center" type="text" name="imc" id="imc" maxlength="4" </p> </div> <div class="col-12 h-90">
-											<input class="w-100 h-50" type="text" name="info" id="info">
-											<img class="w-100 h-50" src="img/IMC.png" />
-											<!--image-->
-									</div>
-								</row>
-							</div>
-
-							<div class="modal-footer bg-dark">
-								<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-							</div>
-						</div>
-					</div>
-				</div>
 			</div>
-		</div>
-	</div>
 	</div>
 	<!-- Bootstrap CSS -->
 	<script src="js/jquery/jquery-3.4.1.min.js"></script>
