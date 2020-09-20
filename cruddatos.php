b0f7079e441bce31d40d4da58d35d5923915bade
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-09-20 12:25:40 -0300

    refact cruddatos

--- cruddatos.php
+++ cruddatos.php
@@ -1,25 +1,16 @@
 <?php
-
 include("dbconnectionsimple.php");
 include_once("persona.php");
 
-//session_start();
-
-function mostrarDatos(){
-  
-  $con = conectar();
-  //if ($con){
-  //por ahora mostramos todas las personas para probar, pero solo mostraria los datos de una persona
-  
-  //El problema de datos es que, cuando una persona ingresa al sistema no tiene datos, y nosotros estabamos intentando mostrarle los datos.
-  //deberíamos tener en cuenta si la persona no tiene datos, y si es así, pedirle q llene sus datos en lugar de intentar mostrarselos
-  
-  //SELECT * FROM `personas` ORDER BY id DESC LIMIT 1
+function mostrarDatosPersona(){
+  // Muestra Los ultimos datos ingresados en la session de usuario
+  // Ordenando descendente y lo limita al último ingreso.
   $sql = 'SELECT * FROM personas WHERE id_users=\''.$_SESSION['id'].'\' ORDER BY id DESC LIMIT 1';
-  //echo $sql;
-  if($con -> query($sql)){
-    $listadepersonas = [];
+  $con = conectar();// dbconnectionsimple.php funtion
+  if($con -> query($sql)){ // check consulta ok
+    $datosActualizados = [];
     $result = $con->query($sql);
+    
     if($result->num_rows > 0){
       while($row = $result->fetch_assoc()) {
         $myPersona = new Persona();
@@ -30,27 +21,23 @@ function mostrarDatos(){
         $myPersona->setNombre($_SESSION['nombre']);
         $myPersona->setApellido($_SESSION['apellido']);
         $myPersona->setFecha($row['fecha']);
-        $listadepersonas[]=$myPersona;
-      }
+        $datosActualizados[]=$myPersona;}
     }else{
-		echo "No tiene datos";
-  }
-  //en lugar de una lista debería devolver el objeto mypersona ya que no se puede hacer una lista con la query
-    return $listadepersonas;
+		  echo "Ingresar datos...";
+    }
+    //en lugar de una lista debería devolver el objeto mypersona ya que no se puede hacer una lista con la query
+    return $datosActualizados;
   }else{
-    echo "No se pudo hacer la query de mostrarDatos";
+    echo " DB Error No se pudo realizar la consulta: mostrarDatosPersona()";
   }
-  //}
 }
 
-function renovarDatos($peso,$altura,$edad,$sessionid){
+function renovarDatos($peso,$altura,$edad,$sessionid,$fecha){
+  // setear fecha actualizada, en la consulta
+  //$fecha = date("Y-m-d H:i:s", strtotime($string));
+  $sql = "insert into personas(peso,altura,edad,id_users,fecha) values($peso,$altura,$edad,$sessionid,$fecha)";
   $con = conectar();
-
-  //aca hay que obtener la fecha actual, guardarla, y meterla en el insert, la query quedaría así
-  //$sql = "insert into personas(peso,altura,edad,id_users,fecha) values($peso,$altura,$edad,$sessionid,$fecha)";
-
-  $sql = "insert into personas(peso,altura,edad,id_users) values($peso,$altura,$edad,$sessionid)";
-
+  
   if($peso = "" || $peso == null){
     echo "<script>alert('Debes ingresar valores para poder actualizar tus datos')</script>";
   }else{
@@ -58,16 +45,8 @@ function renovarDatos($peso,$altura,$edad,$sessionid){
       echo "<script>alert('Datos actualizados exitosamente')</script>";
     } else {
       echo ("<script>alert('Error al actualizar la tarea: " + $con->connect_error + ")</script>");
+    }
   }
-  
+  echo "<script>document.location = 'datos.php';</script>";
 }
-echo "<script>document.location = 'datos.php';</script>";
-  
-
-}
-/*
-INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `peso`, `altura`) VALUES
-(1, 'Miguel', 'Tato', '30', '80', '180'),
-(2, 'Anuj', 'Kumar', '19', '73', '190',);
-*/
 ?>
