d3dbac4ee7b4016b55796b77ccd527d1d567296d
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-09-30 15:27:54 -0300

    refact format

--- persona.php
+++ persona.php
@@ -1,7 +1,6 @@
 <?php
-
   class Persona{
-
+    
     private $id;
     private $peso;
     private $altura;
@@ -10,60 +9,25 @@
     private $nombre;
     private $apellido;
 
-    public function getid(){
-      return $this->id;
-    }
+    public function getid(){return $this->id;}
+    public function setid($id){$this->id = $id;}
 
-    public function setid($id){
-      $this->id = $id;
-    }
+    public function getNombre(){return $this->nombre;}
+    public function setNombre($nombre){$this->nombre = $nombre;}
 
-    public function getNombre(){
-      return $this->nombre;
-    }
+    public function getApellido(){return $this->apellido;}
+    public function setApellido($apellido){$this->apellido = $apellido;}
+   
+    public function getpeso(){return $this->peso;}
+    public function setpeso($peso){$this->peso = $peso;}
 
-    public function setNombre($nombre){
-      $this->nombre = $nombre;
-    }
+    public function getaltura(){return $this->altura;}
+    public function setaltura($altura){$this->altura = $altura;}
 
-    public function getApellido(){
-      return $this->apellido;
-    }
-
-    public function setApellido($apellido){
-      $this->apellido = $apellido;
-    }
-    public function getpeso(){
-      return $this->peso;
-    }
-
-    public function setpeso($peso){
-      $this->peso = $peso;
-    }
-
-    public function getaltura(){
-      return $this->altura;
-    }
-
-    public function setaltura($altura){
-      $this->altura = $altura;
-    }
-
-    public function getedad(){
-      return $this->edad;
-    }
-
-    public function setedad($edad){
-      $this->edad = $edad;
-    }
+    public function getedad(){return $this->edad;}
+    public function setedad($edad){$this->edad = $edad;}
     
-    public function getfecha(){
-      return $this->fecha;
-    }
-    public function setfecha($fecha){
-      $this->fecha = $fecha;
-    }
-
+    public function getfecha(){return $this->fecha;}
+    public function setfecha($fecha){$this->fecha = $fecha;}
   }
-
 ?>
--- rutina.php
+++ rutina.php
@@ -1,43 +1,37 @@
 <?php
-
   class Rutina{
-
     private $id;
+    private $idTRutinas;
     private $nombre;
-    private $intensidad;
-    private $codigo;
-
-    public function getid(){
-      return $this->id;
-    }
-
-    public function setid($id){
-      $this->id = $id;
-    }
-
-    public function getNombre(){
-      return $this->nombre;
-    }
-
-    public function setNombre($nombre){
-      $this->nombre = $nombre;
-    }
-
-    public function getIntensidad(){
-      return $this->intensidad;
-    }
-
-    public function setIntensidad($intensidad){
-      $this->intensidad = $intensidad;
-    }
-
-    public function getCodigo(){
-      return $this->codigo;
-    }
-
-    public function setCodigo($codigo){
-      $this->codigo = $codigo;
-    }
+    private $dia;
+    private $idEjercicio;
+    private $serie;
+    private $repeticiones;
+    private $descanso;
+    //getters y seters
+    //__id______________
+    public function getid(){return $this->id;}
+    public function setId($id){$this->id = $id;}
+    //_id tipo de rutina___
+    public function getIdTipoDeRutinas($idTRutinas){return $this->idTRutinas;} 
+    public function setIdTipoDeRutinas($idTRutinas){$this->idTRutinas = $idTRutinas;}
+    //____Nombre_____
+    public function getNombre(){return $this->nombre;}
+    public function setNombre($nombre){$this->nombre = $nombre;}
+    //____Dia_____
+    public function getDia(){return $this->dia;}
+    public function setDia($dia){$this->dia = $dia;}
+    //____ejercicio
+    public function getIdEjercicio(){return $this->idEjercicio;}
+    public function setIdEjercicio($idEjercicio){$this->idEjercicio = $idEjercicio;}
+    //____Serie___
+    public function getSerie(){return $this->serie;}
+    public function setSerie($serie){$this->serie = $serie;}
+    //___Repeticiones___
+    public function getRepeticiones(){return $this->repeticiones;}
+    public function setRepeticiones($repeticiones){$this->repeticiones = $repeticiones;}
+    //_____Descanso_____
+    public function getDescanso(){return $this->descanso;}
+    public function setDescanso($descanso){$this->descanso = $descanso;}
   }
-
 ?>
