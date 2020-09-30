674b20af0805dab10022fe66bba6c7a8ee151145
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-09-30 15:19:36 -0300

    new columns on rutina table
    change crud rutina for the new columns, and fix bugs

--- crudrutinas.php
+++ crudrutinas.php
@@ -3,21 +3,23 @@ include("dbconnectionsimple.php");
 include_once("rutina.php");
 
 function mapearRutinas(){
-  // Muestra Los ultimos datos ingresados en la session de usuario
-  // Ordenando descendente y lo limita al último ingreso.
-  $sql = 'SELECT * FROM rutinas';
+
+  $sql = 'SELECT * FROM rutina';
   $con = conectar();// dbconnectionsimple.php funtion
   if($con -> query($sql)){ // check consulta ok
-    $datosActualizados = [];
+    $rutinasActualizadas = [];
     $result = $con->query($sql);
     
     if($result->num_rows > 0){
       while($row = $result->fetch_assoc()) {
         $myRutina = new Rutina();
-        $myRutina->setId($row['id']);
-        $myRutina->setId($row['nombre']);
-        $myRutina->setId($row['intensidad']);
-        $myRutina->setId($row['codigo']);
+        $myRutina->setId($row['id_rutinas']);
+        $myRutina->setIdTipoDeRutinas($row['id_rutinas']);
+        $myRutina->setDia($row['numero_de_dia']);
+        $myRutina->setIdEjercicio($row['id_ejercicio']);
+        $myRutina->setSerie($row['series']);
+        $myRutina->setRepeticiones($row['repeticiones']);
+        $myRutina->setDescanso($row['descanso']);
         $rutinasActualizadas[]=$myRutina;}
     }else{
 		  echo "Ingresar datos...";
@@ -25,7 +27,7 @@ function mapearRutinas(){
     //en lugar de una lista debería devolver el objeto myRutina ya que no se puede hacer una lista con la query
     return $rutinasActualizadas;
   }else{
-    echo " DB Error No se pudo realizar la consulta: mostrarDatosRutina()";
+    echo " DB Error No se pudo realizar la consulta: mapearRutinas()";
   }
 }
 function mostrarEjerciciosYRutinas(){
