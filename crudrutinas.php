662af5e16efe8f2338852511ac3d2912feb544e3
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-10-04 00:42:17 -0300

    Update crudrutinas.php
    Fix errors in the query, del unnecessary seters

--- crudrutinas.php
+++ crudrutinas.php
@@ -4,13 +4,12 @@ include_once("rutina.php");
 
 function mapearRutinas($idUrl){
 
-  
-  $sql = 'select * from dietas e, rutinas r, listaddietass x
+  $sql = 'select e.nombre, e.musculo, x.numero_de_dia, x.series, x.repeticiones, x.descanso
+  from ejercicio e, rutinas r, listadejercicios x
   where x.id_rutinas = r.id
-  and x.id_dietas = e.id
+  and x.id_ejercicio = e.id
   and x.id_rutinas = '.$idUrl.'';
-  //echo $sql;
-
+  
   $con = conectar();// dbconnectionsimple.php funtion
   if($con -> query($sql)){ // check consulta ok
     $rutinasActualizadas = [];
@@ -19,15 +18,14 @@ function mapearRutinas($idUrl){
     if($result->num_rows > 0){
       while($row = $result->fetch_assoc()) {
         $myRutina = new Rutina();
-        $myRutina->setId($row['id']);
-        $myRutina->setIdTipoDeRutinas($row['id_rutinas']);
+        $myRutina->setNombre($row['nombre']);
+        $myRutina->setMusculo($row['musculo']);
         $myRutina->setDia($row['numero_de_dia']);
-        $myRutina->setIddietas($row['id_dietas']);
         $myRutina->setSerie($row['series']);
         $myRutina->setRepeticiones($row['repeticiones']);
         $myRutina->setDescanso($row['descanso']);
-        $myRutina->setNombre($row['nombre']);
-        $myRutina->setMusculo($row['musculo']);
+
+
         $rutinasActualizadas[]=$myRutina;}
     }else{
 		  echo "Ingresar datos...";
