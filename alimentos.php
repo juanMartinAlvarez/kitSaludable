d5d2c8acc29ba5df65e2f412e2096c0ce3f4b545
Fernando <fernandolamasw@hotmail.com>
2020-10-06 18:07:27 -0300

    Cambios de formato en alimentos y ejercicios con historial preparado

--- alimentos.php
+++ alimentos.php
@@ -9,89 +9,167 @@ if ($varsesion == null || $varsesion = '') {
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.css" media="all" rel="stylesheet">
-    <link rel="stylesheet" href="estilos/estilosAlimentos.css">
-    <script src="js/jquery/jquery-3.4.1.min.js"></script>
-    <script src="js/jquery/jquery-3.4.1.slim.min.js"></script>
-    <script src="js/popper/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 
-    <title>Dietas</title>
-    <script>
-      $(function(){
-        $('#select-dietas').on('change', function() {
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap.css" media="all" rel="stylesheet">
+  <link rel="stylesheet" href="estilos/estilosAlimentos.css">
+  <script src="js/jquery/jquery-3.4.1.min.js"></script>
+  <script src="js/jquery/jquery-3.4.1.slim.min.js"></script>
+  <script src="js/popper/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+  <script>
+    function appendHistory() {
+      var url = window.location.href,
+        separator = (url.indexOf("?") === -1) ? "?" : "&",
+        newParam = separator + "appendHistory";
+      newUrl = url.replace(newParam, "");
+      newUrl += newParam;
+      window.location.href = newUrl;
+    }
+  </script>
+
+  <title>Dietas</title>
+  <script>
+    $(function() {
+      $('#select-dietas').on('change', function() {
         $('#the-link').attr('href', '?' + $(this).val());
-        });
       });
-    </script>
-  </head>
-  
-  <body>
-    <header>
-      <h1 onclick="location.href='menuprincipal.php'">KitSaludable</h1>
-    </header>
-    <!-- Row 1 Select Dieta -->
-    <div id="contenido1">
-      <div id="row1">
-        <h3 id="tRow1">DIETAS:</h3>
-        <select id="select-dietas" multiple>
-          <option disable selected>--Seleccione una Dieta--</option>
+    });
+  </script>
+</head>
+
+<body>
+  <header>
+    <h1 onclick="location.href='menuprincipal.php'">KitSaludable</h1>
+  </header>
+  <!-- Row 1 Select Dieta -->
+  <div id="contenido1">
+    <div id="row1">
+      <h3 id="tRow1">DIETAS:</h3>
+      <select id="select-dietas" multiple>
+        <option disable selected>--Seleccione una Dieta--</option>
+        <?php
+        require('cruddietas.php');
+        require_once('dbconnectionsimple.php');
+        $sqlDietas = 'SELECT * FROM dietas';
+        $con = conectar(); // dbconnectionsimple.php funtion
+        if ($con->query($sqlDietas)) { // check consulta ok
+          $result = $con->query($sqlDietas);
+          if ($result->num_rows > 0) {
+            foreach ($result as $opciones) : ?>
+              <option value="<?php echo $opciones['id']; ?>"><?php echo $opciones['nombre']; ?></option><?php
+                                                                                                      endforeach;
+                                                                                                    }
+                                                                                                  }
+                                                                                                        ?>
+      </select>
+
+      <div>
+        <br></br><a href='?' id='the-link'>Seleccionar</a><br></br>
+        <?php
+        if (isset($_GET['1'])) {
+          $idUrl = "1";
+        }
+        if (isset($_GET['2'])) {
+          $idUrl = "2";
+        }
+        if (isset($_GET['3'])) {
+          $idUrl = "3";
+        }
+        if (isset($_GET['4'])) {
+          $idUrl = "4";
+        }
+        if (isset($_GET['5'])) {
+          $idUrl = "5";
+        }
+        if (isset($_GET['6'])) {
+          $idUrl = "6";
+        }
+        ?>
+      </div>
+      <div>
+        <?php if (isset($idUrl)) { ?><br></br>
+          <buton class="btn btn-primary" id='historial' onclick="appendHistory()"> Guardar dieta
+
+
+
+          </buton><br></br>
           <?php
-          require('cruddietas.php');
-          require_once('dbconnectionsimple.php');
-          $sql = 'SELECT * FROM dietas';
-          $con = conectar(); // dbconnectionsimple.php funtion
-          if ($con->query($sql)) { // check consulta ok
-            $result = $con->query($sql);
-            if ($result->num_rows > 0) {
-              foreach ($result as $opciones):?>
-                <option value="<?php echo $opciones['id']; ?>"><?php echo $opciones['nombre']; ?></option><?php
-              endforeach;
+          $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+          
+          if (strpos($url, '&appendHistory')) {
+            $sqlPersonaExiste = 'select * from personas WHERE id_users=' . $_SESSION['id'] . '';
+            if ($con->query($sqlPersonaExiste)) {
+              $result = $con->query($sqlPersonaExiste);
+              if (!$result->num_rows > 0) {
+                echo "<script>alert('Debes ingresar tus datos antes de guardar una dieta!')</script>";
+              } else {
+                $sqlHistorial = 'update personas SET id_alimentosDietas =' . $idUrl . '
+                WHERE id_users=' . $_SESSION['id'] . ' ORDER BY id DESC LIMIT 1';
+
+                $con = conectar();
+                if ($con->query($sqlHistorial)) { // check consulta ok
+                  $result = $con->query($sqlHistorial);
+                  if ($result) {
+                    echo "<script>alert('Historial de dietas actualizado correctamente')</script>";
+                  }
+                }
               }
             }
+          } else {
+            if (isset($idUrl)) {
+              echo "Dieta establecida, haz click en Guardar dieta para guardarla en el historial";
+            } else {
+              echo "Debes establecer una dieta antes de guardarla en el historial";
+            }
+          }
           ?>
-        </select>
-      
-        <div>
-          <br></br><a href='?' id='the-link'>Seleccionar</a><br></br>
-          <?php 
-            if(isset($_GET['1'])){$idUrl="1";}
-            if(isset($_GET['2'])){$idUrl="2";}
-            if(isset($_GET['3'])){$idUrl="3";}
-            if(isset($_GET['4'])){$idUrl="4";}
-            if(isset($_GET['5'])){$idUrl="5";}
-            if(isset($_GET['6'])){$idUrl="6";}
-          ?>
-          <br></br><buton class="btn btn-primary" id='historial'> Guardar dieta </buton><br></br>
-        </div>
+        <?php
+        }
+        ?>
       </div>
-      <!-- Row 2 Alimentos table -->
-      <div id="row2">
-      <h3 id="tRow2">LISTA DE ALIMENTOS</h3> 
-        <table id='tabla' bgcolor="2c3e50" class="table table-striped table-bordered table-hover">
-          <tr class="success">
-            <th><h2>Nombre</h2></th>
-            <th><h2>Calorias</h2></th>
-            <th><h2>Tipo</h2></th>
-          </tr>
-          <?php
-            if(isset($idUrl)){
-              $fila= mapearDietas($idUrl);
-              foreach($fila as $alimento){?>
-              <tr><td><h2><?php echo $alimento->getNombre(); ?></h2></td>
-                  <td><h2><?php echo $alimento->getCalorias(); ?></h2></td>
-                  <td><h2><?php echo $alimento->getTipo(); ?></h2></td>
-              <?php }
-              }
-          ?>      
-          </tr>
-        </table>
-      </div>
+
     </div>
-    <script src="js/funciones/historial.js"></script>
-  </body>
-</html> 
+    <!-- Row 2 Alimentos table -->
+    <div id="row2">
+      <h3 id="tRow2">LISTA DE ALIMENTOS</h3>
+      <table id='tabla' bgcolor="2c3e50" class="table table-striped table-bordered table-hover">
+        <tr class="success">
+          <th>
+            <h2>Nombre</h2>
+          </th>
+          <th>
+            <h2>Calorias</h2>
+          </th>
+          <th>
+            <h2>Tipo</h2>
+          </th>
+        </tr>
+        <?php
+        if (isset($idUrl)) {
+          $fila = mapearDietas($idUrl);
+          foreach ($fila as $alimento) { ?>
+            <tr>
+              <td>
+                <h2><?php echo $alimento->getNombre(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $alimento->getCalorias(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $alimento->getTipo(); ?></h2>
+              </td>
+          <?php }
+        }
+          ?>
+            </tr>
+      </table>
+    </div>
+  </div>
+</body>
+
+</html>
--- ejercicios.php
+++ ejercicios.php
@@ -9,94 +9,177 @@ if ($varsesion == null || $varsesion = '') {
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.css" media="all" rel="stylesheet">
-    <link rel="stylesheet" href="estilos/estiloEjercicios.css">
-    <script src="js/jquery/jquery-3.4.1.min.js"></script>
-    <script src="js/jquery/jquery-3.4.1.slim.min.js"></script>
-    <script src="js/popper/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 
-    <title>Ejercicios</title>
-    <script>
-      $(function(){
-        $('#select-ejercicios').on('change', function() {
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap.css" media="all" rel="stylesheet">
+  <link rel="stylesheet" href="estilos/estiloEjercicios.css">
+  <script src="js/jquery/jquery-3.4.1.min.js"></script>
+  <script src="js/jquery/jquery-3.4.1.slim.min.js"></script>
+  <script src="js/popper/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+  <title>Ejercicios</title>
+  <script>
+    function appendHistory() {
+      var url = window.location.href,
+        separator = (url.indexOf("?") === -1) ? "?" : "&",
+        newParam = separator + "appendHistory";
+      newUrl = url.replace(newParam, "");
+      newUrl += newParam;
+      window.location.href = newUrl;
+    }
+  </script>
+  <script>
+    $(function() {
+      $('#select-ejercicios').on('change', function() {
         $('#the-link').attr('href', '?' + $(this).val());
-        });
       });
-    </script>
-  </head>
-  
-  <body>
-    <header>
-      <h1 onclick="location.href='menuprincipal.php'">KitSaludable</h1>
-    </header>
+    });
+  </script>
+</head>
 
-    <div id="contenido1">
-      <!-- Row 1 Select rutina -->
-      <div id="row1">
-        <h3 id="tRow1">RUTINAS:</h3>
-        <select id="select-ejercicios" multiple>
-          <option disable selected>--Seleccione una Rutina--</option>
-          <?php
-          require('crudrutinas.php');
-          require_once('dbconnectionsimple.php');
-          $sql = 'SELECT * FROM rutinas';
-          $con = conectar(); // dbconnectionsimple.php funtion
-          if ($con->query($sql)) { // check consulta ok
-            $result = $con->query($sql);
-            if ($result->num_rows > 0) {
-              foreach ($result as $opciones):?>
-                <option value="<?php echo $opciones['id']; ?>"><?php echo $opciones['nombre']; ?></option><?php
-              endforeach;
+<body>
+  <header>
+    <h1 onclick="location.href='menuprincipal.php'">KitSaludable</h1>
+  </header>
+
+  <div id="contenido1">
+    <!-- Row 1 Select rutina -->
+    <div id="row1">
+      <h3 id="tRow1">RUTINAS:</h3>
+      <select id="select-ejercicios" multiple>
+        <option disable selected>--Seleccione una Rutina--</option>
+        <?php
+        require('crudrutinas.php');
+        require_once('dbconnectionsimple.php');
+        $sql = 'SELECT * FROM rutinas';
+        $con = conectar(); // dbconnectionsimple.php funtion
+        if ($con->query($sql)) { // check consulta ok
+          $result = $con->query($sql);
+          if ($result->num_rows > 0) {
+            foreach ($result as $opciones) : ?>
+              <option value="<?php echo $opciones['id']; ?>"><?php echo $opciones['nombre']; ?></option><?php
+                                                                                                      endforeach;
+                                                                                                    }
+                                                                                                  }
+                                                                                                        ?>
+      </select>
+
+      <div>
+        <br></br><a href='?' id='the-link'> Seleccionar</a><br></br>
+        <?php
+        if (isset($_GET['1'])) {
+          $idUrl = "1";
+        }
+        if (isset($_GET['2'])) {
+          $idUrl = "2";
+        }
+        if (isset($_GET['3'])) {
+          $idUrl = "3";
+        }
+        ?>
+        <div>
+          <?php if (isset($idUrl)) { ?><br></br>
+            <buton class="btn btn-primary" id='historial' onclick="appendHistory()"> Guardar rutina
+
+
+
+            </buton><br></br>
+            <?php
+            $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
+            if (strpos($url, '&appendHistory')) {
+              $sqlPersonaExiste = 'select * from personas WHERE id_users=' . $_SESSION['id'] . '';
+              if ($con->query($sqlPersonaExiste)) {
+                $result = $con->query($sqlPersonaExiste);
+                if (!$result->num_rows > 0) {
+                  echo "<script>alert('Debes ingresar tus datos antes de guardar una rutina!')</script>";
+                } else {
+                  $sqlHistorial = 'update personas SET id_ejerciciosRutinas =' . $idUrl . '
+                WHERE id_users=' . $_SESSION['id'] . ' ORDER BY id DESC LIMIT 1';
+
+                  $con = conectar();
+                  if ($con->query($sqlHistorial)) { // check consulta ok
+                    $result = $con->query($sqlHistorial);
+                    if ($result) {
+                      echo "<script>alert('Historial de rutinas actualizado correctamente')</script>";
+                    }
+                  }
+                }
+              }
+            } else {
+              if (isset($idUrl)) {
+                echo "Rutina establecida, haz click en Guardar Rutina para guardarla en el historial";
+              } else {
+                echo "Debes establecer una rutina antes de guardarla en el historial";
               }
             }
+            ?>
+          <?php
+          }
           ?>
-        </select>
-      
-        <div>
-          <br></br><a href='?' id='the-link'> Seleccionar</a><br></br>
-          <?php 
-            if(isset($_GET['1'])){$idUrl="1";}
-            if(isset($_GET['2'])){$idUrl="2";}
-            if(isset($_GET['3'])){$idUrl="3";}
-          ?>
-          <br></br><buton class="btn btn-primary" id='historial'> Guardar Rutina </buton><br></br>
         </div>
       </div>
-      <!-- Row 2 Ejercicios table -->
-      <div id="row2">
-      <h3 id="tRow2">EJERCICIOS</h3> 
-        <table id='tabla' bgcolor="2c3e50" class="table table-striped table-bordered table-hover">
-          <tr class="success">
-            <th><h2>Dia°</h2></th>
-            <th><h2>Musculo</h2></th>
-            <th><h2>Nombre</h2></th>
-            <th><h2>Series</h2></th>
-            <th><h2>Repeticiones</h2></th>
-            <th><h2>Descanso</h2></th>
-          </tr>
-        <!-- Show rutinas-->
-          <?php 
-            if(isset($idUrl)){
-              $fila= mapearRutinas($idUrl);
-              foreach($fila as $rutina){?>
-              <tr><td><h2><?php echo $rutina->getDia(); ?></h2></td>
-                  <td><h2><?php echo $rutina->getMusculo(); ?></h2></td>
-                  <td><h2><?php echo $rutina->getNombre(); ?></h2></td>
-                  <td><h2><?php echo $rutina->getSerie(); ?></h2></td>
-                  <td><h2><?php echo $rutina->getRepeticiones(); ?></h2></td>
-                  <td><h2><?php echo $rutina->getDescanso(); ?></h2></td></tr>
-              <?php }
-              }
-            ?>      
-          </tr>
-        </table>
-      </div>
     </div>
-  <script src="js/funciones/historial.js"></script>
-  </body>
-</html> 
+    <!-- Row 2 Ejercicios table -->
+    <div id="row2">
+      <h3 id="tRow2">EJERCICIOS</h3>
+      <table id='tabla' bgcolor="2c3e50" class="table table-striped table-bordered table-hover">
+        <tr class="success">
+          <th>
+            <h2>Dia°</h2>
+          </th>
+          <th>
+            <h2>Musculo</h2>
+          </th>
+          <th>
+            <h2>Nombre</h2>
+          </th>
+          <th>
+            <h2>Series</h2>
+          </th>
+          <th>
+            <h2>Repeticiones</h2>
+          </th>
+          <th>
+            <h2>Descanso</h2>
+          </th>
+        </tr>
+        <!-- Show rutinas-->
+        <?php
+        if (isset($idUrl)) {
+          $fila = mapearRutinas($idUrl);
+          foreach ($fila as $rutina) { ?>
+            <tr>
+              <td>
+                <h2><?php echo $rutina->getDia(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $rutina->getMusculo(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $rutina->getNombre(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $rutina->getSerie(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $rutina->getRepeticiones(); ?></h2>
+              </td>
+              <td>
+                <h2><?php echo $rutina->getDescanso(); ?></h2>
+              </td>
+            </tr>
+        <?php }
+        }
+        ?>
+        </tr>
+      </table>
+    </div>
+  </div>
+</body>
+
+</html>
