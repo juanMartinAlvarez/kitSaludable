c89c76b6cc4a9c053ff62eab714d3ae5356779a1
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-10-12 03:38:28 -0300

    refactor and redirect to login or registration

---
+++ iniciarSesion.php
@@ -0,0 +1,7 @@
+<html >
+  <link rel="stylesheet" href="estilos/estilosIniciarSesion.css">
+  <div align="center">
+    <img alt="" class= "img-responsive text-center" 
+     onclick="location.href='index.php'" src="img/error401.jpg"/><!--image-->
+  </div>
+</html>
