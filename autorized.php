6ffeaa56c38476fe14f735f395173b9831d25895
martin alvarez <juan_martin_alvarez@yahoo.com.ar>
2020-10-17 00:31:00 -0300

    refact

--- autorized.php
+++ autorized.php
@@ -1,8 +1,6 @@
-
-
 <?php
 error_reporting(0);
-if(!isset($_SESSION))session_start(); 
+if(!isset($_SESSION))session_start();
   $varsesion = $_SESSION['login']; 
 if ($varsesion == null || $varsesion = '') {
   echo file_get_contents("iniciarSesion.php"); 
